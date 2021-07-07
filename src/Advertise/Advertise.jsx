3e8f3f3a81152a43b03a124694e1aad67547a377
Farish3444 <farish.mechanical@gmail.com>
2021-07-08 04:05:06 +0530

    card

--- src/Advertise/Advertise.jsx
+++ src/Advertise/Advertise.jsx
@@ -1,163 +1,40 @@
 import React from 'react'
 import Poster from '../assets/poster.webp';
 import "../NavBar/NavComp.css";
+import {PricingTable, PricingSlot, PricingDetail} from 'react-pricing-table';
 
-// const Advertise = () => {
-//     console.log('on advertise')
-//     return (
-//         <div className='advertise'>
-//             <h2>Advertise at a lowest price and gain more profitss</h2> 
-//       <img src={Poster} style={{marginTop:'5%'}}/>
-//         </div>
-//     )
-// }
+const Advertise=()=>{
+    return(
+<React.Fragment>
 
-// export default Advertise
-
-/*
-React App that takes pricing / billing codes from JS Object (potentiall an external file) and creates pricing cards with toggle to switch between product lineups
-
-Button links, billing codes, pricing, descriptions, etc. all change on toggle
-
-*/
-
-function CardDescription({ title, description }) {
-    return (
-      <div className="card-description">
-        <h2>{title}</h2>
-        <p>{description}</p>
-      </div>
-    );
-  }
-  
-  function CardBilling({ price, recurrency }) {
-    return (
-      <div className="card-billing">
-        <p>
-          <strong className="price">$ {price}</strong>
-          <strong> / mo.</strong>
-        </p>
-        <p>
-          <span className="recurrency">
-            Billed Anually or $ {recurrency}/monthly
-          </span>
-        </p>
-      </div>
-    );
-  }
-  
-  function CardFeatures({ data }) {
-    return (
-      <div className="card-features">
-        <ul>
-          {data.map((item, index) => {
-            return <li key={index}>{item}</li>;
-          })}
-        </ul>
-      </div>
-    );
-  }
-  
-  function CardAction({ clickMe }) {
-    return (
-      <div className="card-action">
-        <button onClick={clickMe}>BUY NOW</button>
-      </div>
-    );
-  }
-  
-  function PricingCard(props) {
-    const {
-      type,
-      title,
-      description,
-      price,
-      recurrency,
-      mostPopular,
-      data,
-      clickMe
-    } = props;
-  
-    return (
-      <div className={`card pricing-card ${type}`}>
-        {mostPopular ? <span className="most-popular">Most Popular</span> : null}
-        <CardDescription title={title} description={description} />
-        <CardBilling price={price} recurrency={recurrency} />
-        <CardFeatures data={data} />
-        <CardAction clickMe={clickMe} />
-      </div>
-    );
-  }
-  
-  function Advertise() {
-    function handleClick() {
-      console.log("Button clicked!");
-    }
-  
-    return (
-      <div className="app-wrapper">
-        {cardsData.map((props) => {
-          return <PricingCard {...props} key={props.id} clickMe={handleClick} />;
-        })}
-      </div>
-    );
-  }
-  
-  const cardsData = [
-    {
-      id: 1,
-      type: "free",
-      title: "Free Plan",
-      description: "Lorem ipsum",
-      price: 19.99,
-      recurrency: 14.99,
-      mostPopular: false,
-      data: ["2TB Storage", "100 E-mails"]
-    },
-    {
-      id: 2,
-      type: "basic",
-      title: "Basic Plan",
-      description: "Lorem ipsum",
-      price: 29.99,
-      recurrency: 24.99,
-      mostPopular: false,
-      data: ["2TB Storage", "200 E-mails", "10 Accounts"]
-    },
-    {
-      id: 3,
-      type: "medium",
-      title: "Medium Plan",
-      description: "Lorem ipsum",
-      price: 69.99,
-      recurrency: 59.99,
-      mostPopular: true,
-      data: ["10TB Storage", "500 E-mails", "20 Accounts", "24/7 Support"]
-    },
-    {
-      id: 4,
-      type: "pro",
-      title: "Pro Plan",
-      description: "Lorem ipsum",
-      price: 99.99,
-      recurrency: 84.99,
-      mostPopular: false,
-      data: [
-        "50TB Storage",
-        "Unlimited E-mails",
-        "Unlimited Accounts",
-        "24/7 Support"
-      ]
-    }
-  ];
-  
-//   const root = document.querySelector('[data-react-app="root"]');
-  
-//   const render = () => {
-//     return ReactDOM.render(<Advertise />, root);
-//   };
-  
-//   render();
-  
+        <PricingTable  highlightColor='#1976D2'>
+    <PricingSlot  buttonText='TRY IT FREE' title='FREE' priceText='$0/month'>
+        <PricingDetail> <b>15</b> projects</PricingDetail>
+        <PricingDetail> <b>5 GB</b> storage</PricingDetail>
+        <PricingDetail> <b>5</b> users</PricingDetail>
+        <PricingDetail strikethrough> <b>Time tracking</b></PricingDetail>
+    </PricingSlot>
+    <PricingSlot highlighted  buttonText='SIGN UP' title='BASIC' priceText='$24/month'>
+        <PricingDetail> <b>35</b> projects</PricingDetail>
+        <PricingDetail> <b>15 GB</b> storage</PricingDetail>
+        <PricingDetail> <b>Unlimited</b> users</PricingDetail>
+        <PricingDetail> <b>Time tracking</b></PricingDetail>
+    </PricingSlot>
+    <PricingSlot   buttonText='SIGN UP' title='PROFESSIONAL' priceText='$99/month'>
+        <PricingDetail> <b>100</b> projects</PricingDetail>
+        <PricingDetail> <b>30 GB</b> storage</PricingDetail>
+        <PricingDetail> <b>Unlimited</b> users</PricingDetail>
+        <PricingDetail> <b>Time tracking</b></PricingDetail>
+    </PricingSlot>
+    <PricingSlot buttonText='SIGN UP' title='ENTERPRISE' priceText='$200/month'>
+        <PricingDetail> <b>Unlimited</b> projects</PricingDetail>
+        <PricingDetail> <b>75 GB</b> storage</PricingDetail>
+        <PricingDetail> <b>Unlimited</b> users</PricingDetail>
+        <PricingDetail> <b>Time tracking</b></PricingDetail>
+    </PricingSlot>
+  </PricingTable>
+</React.Fragment>
+)
+}
 
 export default Advertise
