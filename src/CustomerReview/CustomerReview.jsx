a80485562304d996ec20064cc14037b130a8bb89
ashpak.farish@awanoo.com <ashpak.farish@barunitsolutions.com>
2021-09-08 20:09:08 +0530

    back to work

--- src/CustomerReview/CustomerReview.jsx
+++ src/CustomerReview/CustomerReview.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { makeStyles,Input,InputLabel,InputAdornment,FormControl,TextField,Button } from '@material-ui/core';
+
 import happyCustomer from '../assets/happycustomer.jpeg';
 
+const useStyles = makeStyles((theme) => ({
+    margin: {
+      margin: theme.spacing(11),
+    },
+    Button:{
+        size:'small',
+        margin:theme.spacing(5)
+    }
+
+  }));
+
 const CustomerReview = () => {
+    const classes = useStyles();
     return (
         <div>
             <h2>Customer Point of Views</h2>
@@ -10,6 +24,20 @@ const CustomerReview = () => {
             <img 
                 src={happyCustomer}
             />
+            <br/>
+<FormControl className={classes.margin}>
+        <InputLabel htmlFor="input-with-icon-adornment">Comment Your Reviews of our Product</InputLabel>
+        <Input
+          id="input-with-icon-adornment"
+          startAdornment={
+            <InputAdornment position="start">
+              {/* <AccountCircle /> */}
+            </InputAdornment>    
+          }
+        />
+        <br/>
+        <Button variant="contained" color="primary" size="small" className={classes.Button}>Submit Review</Button>
+</FormControl>
         </div>
     )
 }
